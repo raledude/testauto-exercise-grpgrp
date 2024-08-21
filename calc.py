5749692994a01b7cb3b5edf04c905d33a53ad4a7
Viktor Iwarson <iwarson@hotmail.com>
2024-08-21 14:01:28 +0200

    beginning of something resembling a calculator

---
+++ calc.py
@@ -0,0 +1,35 @@
+def multiplication():
+    pass
+
+def addition():
+    pass
+
+def subtraction():
+    pass
+
+def division():
+    pass
+
+def square_root():
+    pass
+
+
+while True:
+    print("[1] X")
+    print("[2] -")
+    print("[3] +")
+    print("[4] /")
+
+    choice = input("What do you want to do?")
+
+    if choice == "1":
+        pass
+    elif choice == "2":
+        pass
+    elif choice == "3":
+        pass
+    elif choice == "4":
+        pass
+    else:
+        print("Invalid choice")
+
