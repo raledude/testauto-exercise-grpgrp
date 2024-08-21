d6f35cd15b9dcf8a24b96e0fee369ce93f5576b4
OldSaltyCarb <lord.piggy@hotmail.com>
2024-08-21 14:38:02 +0200

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,3 +1,5 @@
+import math
+
 def multiplication():
     pass
 
@@ -11,14 +13,16 @@ def division():
     pass
 
 def square_root():
-    pass
-
+    x = int(input("Type a number you want to know the square root of"))
+    result = math.sqrt(x)
+    print(result)
 
 while True:
     print("[1] X")
     print("[2] -")
     print("[3] +")
     print("[4] /")
+    print("[5] √")
 
     choice = input("What do you want to do?")
 
@@ -30,6 +34,8 @@ while True:
         pass
     elif choice == "4":
         pass
+    elif choice == "5":
+        square_root()
     else:
         print("Invalid choice")
 
